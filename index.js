9ed8c1856d0c2a610805926548bc16930e415a1e
Johnidel <johnidel@aol.com>
2016-03-15 01:05:53 -0400

    deleted useless file

--- index.js
+++
@@ -1,6 +0,0 @@
-window.onload = function sizeCanvas() {
-	var canvas = document.getElementById("main_canvas");
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight*.7;
-	document.getElementById("title").innerHTML = "fuck";
-}
